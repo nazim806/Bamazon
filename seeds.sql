a7c43d57e7eab4ab0f099fd2faa35d973344c17a
Nazim Uddin <umnazim@gmail.com>
2019-09-15 15:59:58 -0700

    updated Javascript, JSon and MySQL data

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO products ( product_name,  department_name, price, stock_quantity)
+VALUES ("corgis", "pets", 3000.00, 10),
+("pugs", "pets", 2700.00, 10), 
+("llama", "pets", 8000.00, 1),
+("goat", "pets", 9000.00, 15),
+("duck", "pets", 60.00, 20), 
+("turtle", "pets", 150.00, 6),
+("toucan", "pets", 600.00, 12),
+("snake", "pets", 250.00, 8),
+("horse", "pets", 10500, 5)
+("penguin", "pets", 100.00, 5);
